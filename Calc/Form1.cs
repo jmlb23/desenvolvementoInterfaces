cdd99705e34f981e5ef551d0344db73f77a42d84
rmmenosrf24 <rmmenosrf@riseup.net>
2015-09-29 23:00:55 +0200

    calculadora

---
+++ Calc/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calc
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            //handlers para os botons de numeros
+            this.btnUn.Click += new System.EventHandler(Numeros);
+            this.btnDous.Click += new System.EventHandler(Numeros);
+            this.btnTres.Click += new System.EventHandler(Numeros);
+            this.btnCatro.Click += new System.EventHandler(Numeros);
+            this.btnCinco.Click += new System.EventHandler(Numeros);
+            this.btnSeis.Click += new System.EventHandler(Numeros);
+            this.btnSete.Click += new System.EventHandler(Numeros);
+            this.btnOito.Click += new System.EventHandler(Numeros);
+            this.btnNove.Click += new System.EventHandler(Numeros);
+
+            //handlers para os signos
+            this.btnMais.Click += new System.EventHandler(Suma);
+            this.txtOperacion.Validating += new System.ComponentModel.CancelEventHandler(Valida);
+        }
+        //funcion asociada o delgate Click 
+        public void Numeros(object o, EventArgs e)
+        {
+            Button b = (Button)o;
+            txtOperacion.Text += b.Text;
+        }
+
+        public void Suma(object o, EventArgs e)
+        {
+            
+        }
+
+        public void Valida(object o, CancelEventArgs e)
+        {
+            double resultado = 0.0;
+            //parametro out conserva o valor despois do retorno do ambito da funcion
+            if (!double.TryParse(this.txtOperacion.Text, out resultado))
+            {
+                MessageBox.Show("so valores numericos validos");
+            }
+        }
+
+    }
+}
---
+++ proba/proba/FrmEjerc.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.CSharp;
+namespace proba
+{
+    public partial class FrmEjerc : Form
+    {
+        public FrmEjerc()
+        {
+            InitializeComponent();
+        }
+        
+        private void FrmEjerc_Load(object sender, EventArgs e)
+        {
+        
+        }
+
+        private void operacion(object sender, EventArgs e) {
+            Button b = (Button)sender;
+            switch(b.Text){
+                case "+":
+                    if (txtUn.Text.Equals("")  || txtDous.Text.Equals(""))
+                    {
+                        MessageBox.Show("METE VALORES");
+                    }
+                    else 
+                    {
+                        lblResult.Text = string.Format("{0}", (double.Parse(txtUn.Text) + double.Parse(txtDous.Text)));
+                    }
+                    break;
+                case "-":
+                    if (txtUn.Text.Equals("") || txtDous.Text.Equals(""))
+                    {
+                        MessageBox.Show("METE VALORES");
+                    }
+                    else
+                    {
+                        lblResult.Text = string.Format("{0}", (double.Parse(txtUn.Text) - double.Parse(txtDous.Text)));
+                    }
+                    break;
+                case "*":
+                    if (txtUn.Text.Equals("") || txtDous.Text.Equals(""))
+                    {
+                        MessageBox.Show("METE VALORES");
+                    }
+                    else
+                    {
+                        lblResult.Text = string.Format("{0}", (double.Parse(txtUn.Text) * double.Parse(txtDous.Text)));
+                    }
+                    break;
+                case "/":
+                    if (txtUn.Text.Equals("") || txtDous.Text.Equals(""))
+                    {
+                        MessageBox.Show("METE VALORES");
+                    }
+                    else
+                    {
+                        lblResult.Text = string.Format("{0}",(double.Parse(txtUn.Text) / double.Parse(txtDous.Text)));
+                    }
+                    break;
+            }
+        }
+    }
+}
